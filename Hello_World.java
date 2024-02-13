f07813331eab7a400c8a74da12fa9229b350c912
Dazeguy <shane.yang@yahoo.com>
2024-02-12 19:03:03 -0500

    Create Hello_World.java
    I moved a few of the files around.

---
+++ Hello_World.java
@@ -0,0 +1,18 @@
+public class Hello_World
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+      
+      //name is a reference variable
+      String name = "George Washington";
+      
+      System.ou.println(name);
+      
+      //age is a primitive variable
+      int age = 24;
+      
+      System.out.println(age);
+      
+   }
+}
